80dd9286294bc38ce69fe03b54548e588686141b
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2025-03-17 12:24:37 +0530

    ts

---
+++ JS-All/class-4-controllFlow/one.js
@@ -0,0 +1,8 @@
+let num = 450;
+
+if(num%2 ==0){
+    console.log("Even Number")
+}
+else{
+    console.log("Odd Number")
+}
---
+++ JS-All/class-4-controllFlow/two.js
@@ -0,0 +1,9 @@
+let input = require('prompt-sync')();
+let num = parseInt(input('Enter Number:'));
+
+if(num%2 ==0){
+    console.log("Even Number")
+}
+else{
+    console.log("Odd Number")
+}
