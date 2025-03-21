cfaa859889a5594cd7108bc0d62cbd6ea75d66ae
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2025-03-21 12:31:03 +0530

    ts

---
+++ JS-All/class-8-JSEventsDOM/Assignment/colors.js
@@ -0,0 +1,5 @@
+function changeColor(){
+    document.querySelector('.btn1').style.backgroundColor="green"
+    //document.querySelector('.btn1').innerHTML="GM"
+    //alert("Test Case 123")
+}
---
+++ JS-All/class-8-JSEventsDOM/DOM1/data.js
@@ -0,0 +1,5 @@
+/* let p_Tag_Ref=document.getElementById('abc')
+
+console.log(p_Tag_Ref) */
+document.getElementById('abc').innerHTML="GM"
+document.getElementsByTagName('button')[0].innerHTML="Logout"
